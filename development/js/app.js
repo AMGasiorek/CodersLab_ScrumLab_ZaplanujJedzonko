20d5f66c102e206b012e57f188430f77d48dccc4
Adam <adam.rurek@gmail.com>
2019-07-31 09:47:14 +0200

    Appsection4

--- development/js/app.js
+++ development/js/app.js
@@ -2,8 +2,25 @@ console.clear();
 
 var userButton = document.querySelector('#userButton');
 
+
 userButton.addEventListener('click', function(){
-    var userName = document.querySelector('#userNameInput').value
+    var userName = document.querySelector('#userNameInput').value;
     localStorage.setItem('savedName', userName);
     console.log("Twoje imię to: ", localStorage.savedName);
-})
+
+    document.querySelector('header .name span').innerText = userName;
+    document.querySelector('.appsection').style.display = "none";
+
+});
+
+document.addEventListener('DOMContentLoaded', function (){
+
+    if (localStorage.getItem("savedName") !== null) {
+        document.querySelector('header .name span').innerText = localStorage.getItem("savedName");
+        document.querySelector('.appsection').style.display = "none";
+    } else {
+        document.querySelector('.appsection').style.display = "block";
+    }
+
+
+});
