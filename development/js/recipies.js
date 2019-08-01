81a7f1145622b131719e11a142cb281261189ff6
Adam <adam.rurek@gmail.com>
2019-08-01 14:58:49 +0200

    Recipiestable

---
+++ development/js/recipies.js
@@ -0,0 +1,31 @@
+console.clear();
+
+var userButton = document.querySelector('#userButton');
+
+
+userButton.addEventListener('click', function(){
+    var userName = document.querySelector('#userNameInput').value;
+    localStorage.setItem('savedName', userName);
+    // console.log("Twoje imię to: ", localStorage.savedName);
+
+    document.querySelector('header .name span').innerText = userName;
+    document.querySelector('.appsection').style.display = "none";
+    document.querySelector('.recipysection').style.display = "flex";
+
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log(document.querySelector('.appsection'));
+    console.log(document.querySelector('.recipysection'));
+
+    if (localStorage.getItem("savedName") !== null) {
+        document.querySelector('header .name span').innerText = localStorage.getItem("savedName");
+        document.querySelector('.appsection').style.display = "none";
+        document.querySelector('.recipysection').style.display = "flex";
+    } else {
+        document.querySelector('header .name span').innerText = "Imię";
+        document.querySelector('.appsection').style.display = "flex";
+        document.querySelector('.recipysection').style.display = "none";
+    }
+
+});
