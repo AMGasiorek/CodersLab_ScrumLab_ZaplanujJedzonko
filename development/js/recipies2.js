b99896ede6c48ae84324a7c35f92a5806c1c0f03
AMGasiorek <a.m.gasiorek@gmail.com>
2019-08-02 10:49:41 +0200

    Edytowanie przepisów zrobione

--- development/js/recipies2.js
+++ development/js/recipies2.js
@@ -1,7 +1,6 @@
 
 var addRecipe = document.getElementById("add-recipe");
 
-var allRecipies = {};
 
 var recipeName = document.getElementById("recipe-name");
 var recipeDescription = document.getElementById("recipe-description");
@@ -17,34 +16,109 @@ var ingredientsList = document.getElementById("ingredients");
 
 
 addInstructionButton.addEventListener("click", function () {
+
+    var spanTxt = document.createElement("span");
+
+    var editButton = document.createElement("BUTTON");
+    var claEdi = document.createAttribute("class");
+    claEdi.value = "edit-button far fa-edit";
+    editButton.setAttributeNode(claEdi);
+
+    var deleteButton = document.createElement("BUTTON");
+    var claDel = document.createAttribute("class");
+    claDel.value = "delete-button far fa-trash-alt";
+    deleteButton.setAttributeNode(claDel);
+
+
+
     var newLi = document.createElement("li");
-    newLi.textContent = newInstruction.value;
-    instructionsList.append(newLi);
+
+    spanTxt.innerText = newInstruction.value;
+    newLi.appendChild(spanTxt);
+    newLi.appendChild(deleteButton);
+    newLi.appendChild(editButton);
+    instructionsList.appendChild(newLi);
+
     newInstruction.value = "";
+
+    deleteButton.addEventListener("click",function () {
+        this.parentElement.parentElement.removeChild(this.parentElement);
+    });
+    editButton.addEventListener("click",function () {
+        this.parentElement.querySelector("span").setAttribute("contenteditable","true");
+    });
+    spanTxt.addEventListener("blur",function () {
+        this.setAttribute("contenteditable","false");
+    })
 });
 
 addIngredientsButton.addEventListener("click", function () {
+
+    var spanTxt = document.createElement("span");
+
+    var editButton = document.createElement("BUTTON");
+    var claEdi = document.createAttribute("class");
+    claEdi.value = "edit-button far fa-edit";
+    editButton.setAttributeNode(claEdi);
+
+    var deleteButton = document.createElement("BUTTON");
+    var claDel = document.createAttribute("class");
+    claDel.value = "delete-button far fa-trash-alt";
+    deleteButton.setAttributeNode(claDel);
+
     var newLi = document.createElement("li");
-    newLi.textContent = newIngredient.value;
-    ingredientsList.append(newLi);
+
+    spanTxt.innerText = newIngredient.value;
+    newLi.appendChild(spanTxt);
+    newLi.appendChild(deleteButton);
+    newLi.appendChild(editButton);
+    ingredientsList.appendChild(newLi);
+
     newIngredient.value = "";
+
+    deleteButton.addEventListener("click",function () {
+        this.parentElement.parentElement.removeChild(this.parentElement);
+    });
+    editButton.addEventListener("click",function () {
+        this.parentElement.querySelector("span").setAttribute("contenteditable","true");
+    });
+    spanTxt.addEventListener("blur",function () {
+        this.setAttribute("contenteditable","false");
+    })
 });
 
+
 addRecipe.addEventListener("click", function(){
+    var allRecipies = JSON.parse(localStorage.getItem("recipiesZJ"));
     var instructionsTab = [];
     var ingredientsTab = [];
     var instructionsListElements = instructionsList.getElementsByTagName("li");
     var ingredientsListElements = ingredientsList.getElementsByTagName("li");
 
-    for (i=0; i<ingredientsListElements.length; i++) {
-        ingredientsTab.push(ingredientsListElements[i].innerText);
+    for (var i=0; i<ingredientsListElements.length; i++) {
+        ingredientsTab.push(ingredientsListElements[i].querySelector("span").innerText);
     }
-    for (i=0; i<instructionsListElements.length; i++) {
-        instructionsTab.push(instructionsListElements[i].innerText);
+    for (var i=0; i<instructionsListElements.length; i++) {
+        instructionsTab.push(instructionsListElements[i].querySelector("span").innerText);
     }
 
+
+
     allRecipies[recipeName.value]={description: recipeDescription.value, instructions: instructionsTab, ingredients: ingredientsTab};
-    console.log(allRecipies);
-    localStorage.setItem("recipiesZJ", allRecipies);
-    console.log(localStorage.getItem("recipiesZJ"));
+    localStorage.setItem("recipiesZJ", JSON.stringify(allRecipies));
+
+
+
+    var ingredientsListLength=ingredientsListElements.length;
+    for (var i=0; i<ingredientsListLength; i++) {
+        ingredientsListElements[0].parentElement.removeChild(ingredientsListElements[0])
+    }
+
+    var instructionsListLength=instructionsListElements.length;
+    for (var i=0; i<instructionsListLength; i++) {
+        instructionsListElements[0].parentElement.removeChild(instructionsListElements[0])
+    }
+
+    recipeName.value="";
+    recipeDescription.value="";
 });
