58a7e77c9f3ab949ebaa68750e966f36c666931c
oskartyt <tytusoskar@gmail.com>
2019-08-02 16:56:45 +0200

    adding plan done

--- development/js/schedules.js
+++ development/js/schedules.js
@@ -26,4 +26,48 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector('.add__plan').style.display = "none";
     }
 
+    if (localStorage.getItem("recipiesZJ")==undefined){
+        var allRecipies = {};
+    }else{
+        var allRecipies = JSON.parse(localStorage.getItem("recipiesZJ"));
+    }
+    var recepiesKeys=Object.keys(allRecipies);
+    var newPlanSelects=document.getElementsByClassName("newPlanTable")[0].getElementsByTagName("select");
+    for (var i=0;i<newPlanSelects.length;i++){
+        var actualSelect=newPlanSelects[i];
+        for (var j=0;j<recepiesKeys.length;j++){
+            var optionRecipe = document.createElement("option");
+            optionRecipe.setAttribute("value",recepiesKeys[j]);
+            optionRecipe.innerText=recepiesKeys[j];
+            actualSelect.appendChild(optionRecipe);
+        }
+    }
+});
+
+var addPlanButton=document.querySelector(".add__plan__button");
+
+
+addPlanButton.addEventListener("click",function () {
+
+    if (localStorage.getItem("plansZJ")==undefined){
+        var allPlans = {};
+    }else{
+        var allPlans = JSON.parse(localStorage.getItem("plansZJ"));
+    }
+
+    var nameInput=document.querySelector(".newPlanName");
+    var descriptionInput=document.querySelector(".newPlanDescription");
+    var numberInput=document.querySelector(".newPlanWeekNr");
+
+    var PlanSelects=document.getElementsByClassName("newPlanTable")[0].getElementsByTagName("select");
+    allPlans[numberInput.value]={"name":nameInput.value,"description":descriptionInput.value,"meals":[]};
+    for (var i=0;i<PlanSelects.length;i++){
+        var meal=PlanSelects[i].dataset.meal;
+        var day=PlanSelects[i].parentElement.parentElement.querySelector("th").dataset.day;
+        var chosenRecipe=PlanSelects[i].value;
+
+        var oneElementTable=[meal,day,chosenRecipe];
+        allPlans[numberInput.value]["meals"].push(oneElementTable);
+    }
+    localStorage.setItem("plansZJ", JSON.stringify(allPlans));
 });
