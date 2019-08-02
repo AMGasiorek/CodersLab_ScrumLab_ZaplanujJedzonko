deeb1a035e7f93ba3a609be4ec40e39390d4fe04
Ziemecka <barbara.ziemecka@interia.pl>
2019-08-02 18:01:41 +0200

    dodawanie planów

--- development/js/app.js
+++ development/js/app.js
@@ -27,6 +27,35 @@ document.addEventListener('DOMContentLoaded', function (){
     }
 
 
+    var plansTableCells=document.querySelector(".planView").querySelectorAll("td");
+
+    if (localStorage.getItem("plansZJ")!==undefined && localStorage.getItem("recipiesZJ")!==null) {
+        var plans= JSON.parse(localStorage.getItem("plansZJ"));
+        var firstKey=Object.keys(plans)[0];
+        var meals=plans[firstKey]["meals"];
+
+        for (var i=0;i<plansTableCells.length;i++){
+            for (var j=0;j<meals.length;j++){
+                if (meals[j][0]==plansTableCells[i].parentElement.dataset.meal &&  meals[j][1]==plansTableCells[i].dataset.day){
+                    plansTableCells[i].innerText=meals[j][2];
+                }
+            }
+        }
+
+        var titleNr=document.querySelector(".userPlan").querySelector("span");
+
+        titleNr.innerText=firstKey;
+
+    }
+    if (localStorage.getItem("recipiesZJ")==undefined){
+        var nrOfRecepies = 0;
+    }else{
+        var nrOfRecepies = Object.keys(JSON.parse(localStorage.getItem("recipiesZJ"))).length;
+    }
+    var nrOfRecepiesInfo=document.querySelector("div.blueWidget").querySelector("span");
+
+    nrOfRecepiesInfo.innerText=nrOfRecepies;
+
 });
 
 
@@ -40,6 +69,13 @@ document.addEventListener('DOMContentLoaded', function (){
              parent.style.display = "none";
      });
  }
+
+ var addRecipeButton = document.querySelector(".addWidgetButton.recipe");
+
+addRecipeButton.addEventListener("click", function(){
+     document.querySelector('.desktop').style.display = "none";
+     window.location.href="recipes.html";
+});
 
 
 
